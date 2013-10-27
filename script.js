fea4a1204e9e1d370b6e61bd3540885e9c540f9e
bbarone <ben.baronenugent@gmail.com>
2013-10-27 21:25:29 +1100

    Update script.js

--- script.js
+++ script.js
@@ -51,12 +51,12 @@
 				}
 
 				setInterval(function() {
-					if (Math.sqrt((ax*ax)+(ay*ay)+(az*az)) > 12) {
+					if (Math.sqrt((ax*ax)+(ay*ay)+(az*az)) > 11) {
 						document.getElementById("xlabel").innerHTML = "Hello!";
 						document.getElementById("show").innerHTML = "Number: " + Math.sqrt((ax*ax)+(ay*ay)+(az*az));
 						document.getElementById("display").innerHTML = "Steps: " + ++steps;
 					}
-					if (Math.sqrt((ax*ax)+(ay*ay)+(az*az)) <= 12) {
+					if (Math.sqrt((ax*ax)+(ay*ay)+(az*az)) <= 11) {
 						document.getElementById("xlabel").innerHTML = "Bye!";
 						document.getElementById("show").innerHTML = "Number: " + Math.sqrt((ax*ax)+(ay*ay)+(az*az));
 					}
